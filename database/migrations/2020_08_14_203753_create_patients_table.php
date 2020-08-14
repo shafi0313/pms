cd6a9349550b4933825001e78ca5ad68a52babd1
Shafi <msh.shafiul@gmail.com>
2020-08-14 21:13:44 +0600

    Add Patients Table

---
+++ database/migrations/2020_08_14_203753_create_patients_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePatientsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('patients', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->integer('age');
+            $table->string('gender');
+            $table->string('email')->unique()->nullable();
+            $table->integer('phone');
+            $table->string('address');
+            $table->string('add_by')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('patients');
+    }
+}
